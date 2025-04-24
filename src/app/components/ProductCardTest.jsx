cfffdc4de82032be4556e893d41837c9f10292af
Sofie <Soov0001@stud.kea.dk>
2025-04-24 17:22:28 +0200

    singleview routing på zustand cardet

--- src/app/components/ProductCardTest.jsx
+++ src/app/components/ProductCardTest.jsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useCartStore } from "../store/cartStore";
+import Link from "next/link";
 
 const ProductCardTest = ({ product }) => {
   const addToCart = useCartStore((state) => state.addToCart);
 
   return (
     <div className="p-4 border rounded">
-      <h2>{product.title}</h2>
-      <p>{product.price} kr</p>
+      <Link href={`/products/${product.id}`}>
+        <h2>{product.title}</h2>
+        <p>{product.price} kr</p>
+      </Link>
       <button
         onClick={() => addToCart(product)}
         className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
