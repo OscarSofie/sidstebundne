7bf87ff7b43ecee0fb182d6c3d8f59d087c3cf45
Sofie <Soov0001@stud.kea.dk>
2025-04-28 22:02:50 +0200

    knap ændring

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,26 @@
-
 import Link from "next/link";
 import ProductCardTest from "./components/ProductCardTest";
 import Image from "next/image";
 import Header from "./components/Header";
 
-
 export default function Home() {
   return (
-
     <div>
       <section className="flex flex-col items-center justify-center text-center py-20">
-        <h1 className="text-5xl font-bold mb-4">Alt hvad du mangler – ét sted!</h1>
-        <p className="text-lg text-gray-700 mb-8">Oplev tusindvis af produkter inden for elektronik, skønhed, bolig og meget mere.</p>
-        <Link href="/products" className="bg-black text-white hover:bg-pink-600 transition px-6 py-3 rounded text-lg">
+        <h1 className="text-5xl font-bold mb-4">
+          Alt hvad du mangler – ét sted!
+        </h1>
+        <p className="text-lg text-gray-700 mb-8">
+          Oplev tusindvis af produkter inden for elektronik, skønhed, bolig og
+          meget mere.
+        </p>
+        <Link
+          href="/products"
+          className="bg-black text-white font-semibold hover:bg-sky-800 transition px-6 py-3 rounded-3xl text-lg"
+        >
           Gå til produkter
         </Link>
       </section>
-       
-      </div>
-   
-
-
+    </div>
   );
 }
